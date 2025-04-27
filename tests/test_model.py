c8fe8aa33a177c68b3d45247a71e4eb5bfc2198c
Karthik Prabhakar <knkarthik01@gmail.com>
2025-04-27 05:55:10 -0700

    Resolved LICENSE merge conflict

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import torch
 from models.cnn_dual_branch.model import DualBranchNet
--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from recommendation_engine.recommend_filter import recommend_filter
 
